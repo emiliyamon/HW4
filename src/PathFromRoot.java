b5452817d347125fed83cbeb2a5e021518784716
emiliyamon <emiliyam@campus.technion.ac.il>
2023-06-30 16:22:54 +0300

    3006 1622 PathFromRoot

--- src/PathFromRoot.java
+++ src/PathFromRoot.java
@@ -1,6 +1,13 @@
 public class PathFromRoot {
     public static boolean doesPathExist(BinNode<Character> root, String str) {
-        // TODO: Add your code for part A1 here...
+        if (str.length() == 0) {
+            return true;
+        }
+        if (root.getData() != str.charAt(0)) {
+            return false;
+        }
+        String copy = str.substring(1);
+        return doesPathExist(root.getLeft(), copy) && doesPathExist(root.getRight(), copy);
     }
 
 }
