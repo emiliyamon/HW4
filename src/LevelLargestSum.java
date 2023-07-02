4e52900e1e32bb1c8bcf76b17716a2634d62c6b1
emiliyamon <emiliyam@campus.technion.ac.il>
2023-07-02 11:33:58 +0300

    0207 1133 LevelLargestSum

--- src/LevelLargestSum.java
+++ src/LevelLargestSum.java
@@ -1,5 +1,46 @@
+import java.util.ArrayDeque;
+
 public class LevelLargestSum {
     public static int getLevelWithLargestSum(BinNode<Integer> root) {
-        // TODO: Add your code for part A2 here...
+        if (root == null) {
+            return -1;
+        }
+
+        int maxLevel = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        ArrayDeque<BinNode<Integer>> deque = new ArrayDeque<>();
+        deque.offer(root);
+        int currentLevel = 0;
+
+        while (!deque.isEmpty()) {
+            int currentLevelSize = deque.size();
+            int currentSum = 0;
+
+            for (int i = 0; i < currentLevelSize; i++) {
+                BinNode<Integer> node = deque.poll();
+                currentSum += node.getData();
+
+                if (node.getLeft() != null) {
+                    deque.offer(node.getLeft());
+                }
+
+                if (node.getRight() != null) {
+                    deque.offer(node.getRight());
+                }
+            }
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                maxLevel = currentLevel;
+            }
+
+            currentLevel++;
+
+        }
+
+        return maxLevel;
     }
+
 }
+
